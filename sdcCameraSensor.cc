94a2700c5d4f76298cc2d4f14f15c80192cb8874
selfcar <selfcar@cmc307-04.mathcs.carleton.edu>
2016-02-04 22:45:47 -0600

    updated line detection heuristics but still garbage

--- sdcCameraSensor.cc
+++ sdcCameraSensor.cc
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <vector>
 #include <iostream>
+#include <numeric>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/objdetect/objdetect.hpp"
@@ -47,81 +48,108 @@ void sdcCameraSensor::OnUpdate(){
   // printf("\n\n");
 
   // Kappa
-  const unsigned char* img = this->parentSensor->GetImageData(0);
+  const unsigned char* img_left = this->parentSensor->GetImageData(0);
+  const unsigned char* img_right = this->parentSensor->GetImageData(1);
 
-  Mat image = Mat(this->parentSensor->GetImageHeight(0), this->parentSensor->GetImageWidth(0), CV_8UC3, const_cast<unsigned char*>(img));
-  Mat imageROI = image(cv::Rect(0, image.rows/2, image.cols, image.rows/2));
+  Mat image_left = Mat(this->parentSensor->GetImageHeight(0), this->parentSensor->GetImageWidth(0), CV_8UC3, const_cast<unsigned char*>(img_left));
+  Mat image_right = Mat(this->parentSensor->GetImageHeight(1), this->parentSensor->GetImageWidth(1), CV_8UC3, const_cast<unsigned char*>(img_right));
+
+  Mat imageROI_left = image_left(cv::Rect(0, image_left.rows/2, image_left.cols, image_left.rows/2));
+  Mat imageROI_right = image_right(cv::Rect(0, image_right.rows/2, image_right.cols, image_right.rows/2));
 
   // Canny algorithm
-  Mat contours;
-  Canny(imageROI,contours,50,350);
-  Mat contoursInv;
-  threshold(contours,contoursInv,128,255,THRESH_BINARY_INV);
+  Mat contours_left, contours_right;
+  Canny(imageROI_left,contours_left,50,350);
+  Canny(imageROI_right,contours_right,50,350);
+  Mat contoursInv_left, contoursInv_right;
+  threshold(contours_left,contoursInv_left,128,255,THRESH_BINARY_INV);
+  threshold(contours_right,contoursInv_right,128,255,THRESH_BINARY_INV);
 
   float PI = 3.14159;
-  std::vector<Vec2f> lines;
-  HoughLines(contours,lines,1,PI/180, 100);
-  Mat result(contours.rows,contours.cols,CV_8U,Scalar(255));
-  image.copyTo(result);
+  std::vector<Vec2f> lines_left;
+  std::vector<Vec2f> lines_right;
 
+  HoughLines(contours_left,lines_left,1,PI/180, 100);
+  HoughLines(contours_right,lines_right,1,PI/180, 100);
+  Mat result_left(contours_left.rows,contours_left.cols,CV_8U,Scalar(255));
+  Mat result_right(contours_right.rows,contours_right.cols,CV_8U,Scalar(255));
+  image_left.copyTo(result_left);
+  image_right.copyTo(result_right);
+  //print out line angles
+  //for (std::vector<Vec2f>::const_iterator i = lines_left.begin(); i != lines_left.end(); ++i)
+  //  std::cout << *i << ' ' << std::endl;
+  //std::cout << "=====================================\n";
   // Draw the lines
-  std::vector<Vec2f>::const_iterator it = lines.begin();
-  Mat hough(imageROI.size(),CV_8U,Scalar(0));
+  std::vector<Vec2f>::const_iterator it_left = lines_left.begin();
+  std::vector<Vec2f>::const_iterator it_right = lines_right.begin();
 
-  line(imageROI, Point(0,0), Point(320,0), Scalar(255,255,255), 1);
-  line(imageROI, Point(160,0), Point(160,120), Scalar(255,255,255), 1);
+  line(imageROI_left, Point(0,0), Point(320,0), Scalar(255,255,255), 1);
+  line(imageROI_left, Point(160,0), Point(160,120), Scalar(255,255,255), 1);
+  line(imageROI_right, Point(0,0), Point(320,0), Scalar(255,255,255), 1);
+  line(imageROI_right, Point(160,0), Point(160,120), Scalar(255,255,255), 1);
 
-  while (it!=lines.end()) {
-      float rho= (*it)[0];   // first element is distance rho
-      float theta= (*it)[1]; // second element is angle theta
+
+//this isnt working that well - should classify lines on left and lines on right
+//based on their theta value and then take averages of left and right lines.
+//take the area between the single left and single right line as our area we can
+//go to
+  //iter over left
+  std::vector<float> pt1_x;
+  std::vector<float> pt1_y;
+  std::vector<float> pt2_x;
+  std::vector<float> pt2_y;
+  while (it_left!=lines_left.end()) {
+      float rho= (*it_left)[0];   // first element is distance rho
+      float theta= (*it_left)[1]; // second element is angle theta
       // point of intersection of the line with first row
-      //if (theta < 1.5 || theta > 1.7) {
+      if ( (theta > 0.09 && theta < 1.48) || (theta < 3.14 && theta > 1.66) ){
+      Point pt1(rho/cos(theta),0);
+      pt1_x.push_back(rho/cos(theta));
+      pt1_y.push_back(0);
+      // point of intersection of the line with last row
+      Point pt2((rho-result_left.rows*sin(theta))/cos(theta),result_left.rows);
+      pt2_x.push_back((rho-result_left.rows*sin(theta))/cos(theta));
+      pt2_y.push_back(result_left.rows);
+      // draw a white line
+    }
+      //line(imageROI_left, pt1, pt2, Scalar(255), 3);
+
+      ++it_left;
+  }
+if (pt1_x.size() > 0 && pt1_y.size() > 0 && pt2_x.size() > 0 && pt2_y.size() > 0){
+float pt1_x_average = std::accumulate(pt1_x.begin(), pt1_x.end(), 0)/pt1_x.size();
+float pt1_y_average = std::accumulate(pt1_y.begin(), pt1_y.end(), 0)/pt1_y.size();
+float pt2_x_average = std::accumulate(pt2_x.begin(), pt2_x.end(), 0)/pt2_x.size();
+float pt2_y_average = std::accumulate(pt2_y.begin(), pt2_y.end(), 0)/pt2_y.size();
+line(imageROI_left, Point(pt1_x_average,pt1_y_average), Point(pt2_x_average,pt2_y_average), Scalar(255), 3);
+}
+//iter over right
+  while (it_right!=lines_right.end()) {
+      float rho= (*it_right)[0];   // first element is distance rho
+      float theta= (*it_right)[1]; // second element is angle theta
+      // point of intersection of the line with first row
+      //if (theta < 1.5 || theta > 1.7)
       Point pt1(rho/cos(theta),0);
       // point of intersection of the line with last row
-      Point pt2((rho-result.rows*sin(theta))/cos(theta),result.rows);
+      Point pt2((rho-result_right.rows*sin(theta))/cos(theta),result_right.rows);
       // draw a white line
-      //line(result, pt1, pt2, Scalar(255), 3);
-      line(imageROI, pt1, pt2, Scalar(255), 3);
-      /*
-      //rectangle(imageROI, pt1, Point(theta,result.rows), Scalar(255,255,0), 3,8,0);
-      int thickness = 3;
-      int lineType = 8;
-      int shift = 0;
-      vector<Point> fill_points;
-      fill_points.push_back(Point(160,0));
-      fill_points.push_back(Point(160,imageROI.rows-2));
-      fill_points.push_back(Point(320,imageROI.rows-2));
-      fill_points.push_back(Point(320,imageROI.rows-2));
-      //fill_points.push_back(pt2);
-
-      const Point *ppt = (const Point*) Mat(fill_points).data;
-      int npt = Mat(fill_points).rows;
-      //circle(imageROI, Point(160,0), radius, Scalar(255), thickness, lineType, shift);
-      //fillPoly(imageROI,ppt,&npt,0,1,Scalar(255),thickness,lineType,shift);
-      polylines(imageROI,&ppt,&npt,1,true,Scalar( 255, 255, 255 ),2,lineType,0);
-      */
-
-    //   std::cout << "line: (" << rho << "," << theta << ")\n";
-      //}
-      ++it;
+      line(imageROI_right, pt1, pt2, Scalar(255), 3);
+      ++it_right;
   }
+  /*
   CascadeClassifier stopSign;
   if ( !stopSign.load("haarcascade.xml") ){ printf("--(!)Error loading\n");}
-
   std::vector<Rect> stopsigns;
   //std::cout << "hi\n";
   stopSign.detectMultiScale(image, stopsigns, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30));
   if (stopsigns.size() > 0){
     std::cout << "found a sign!!\n";
   }
-  namedWindow("Lane Detection", CV_WINDOW_AUTOSIZE);
-  imshow("Lane Detection", image);
+  */
+  namedWindow("Lane Detection Left", CV_WINDOW_AUTOSIZE);
+  namedWindow("Lane Detection Right", CV_WINDOW_AUTOSIZE);
+  imshow("Lane Detection Left", image_left);
+  imshow("Lane Detection Right", image_right);
+
   waitKey(4);
-
-  //const unsigned char* img2 = this->parentSensor->GetImageData(1);
-  //Mat frame2 = Mat(this->parentSensor->GetImageHeight(1), this->parentSensor->GetImageWidth(1), CV_8UC3, const_cast<unsigned char*>(img2));
-  //frame.setTo(_InputArray(img));
-  //namedWindow("MyWindow2", CV_WINDOW_AUTOSIZE);
-  //imshow("MyWindow2", frame2);
-
 }
